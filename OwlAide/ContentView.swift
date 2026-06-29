7f2ad772a02abedd53e430b1737b89c679054080
gaozhongkui <gaozhongkui@meetstarinc.com>
2026-06-29 16:43:00 +0800

    init project

---
+++ OwlAide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OwlAide
+//
+//  Created by gaozhongkui on 2026/6/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OwlAide/OwlAideApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwlAideApp.swift
+//  OwlAide
+//
+//  Created by gaozhongkui on 2026/6/29.
+//
+
+import SwiftUI
+
+@main
+struct OwlAideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
